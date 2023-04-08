5108a66b8c5aea362c19259bd0361c0841988fac
YuraSich <39992462+YuraSich@users.noreply.github.com>
2023-04-08 23:37:09 +0700

    Time: 57 ms (97.41%), Space: 40.8 MB (43.10%) - LeetHub

--- 2609-find-the-longest-balanced-substring-of-a-binary-string/2609-find-the-longest-balanced-substring-of-a-binary-string.cs
+++ 2609-find-the-longest-balanced-substring-of-a-binary-string/2609-find-the-longest-balanced-substring-of-a-binary-string.cs
@@ -1,38 +1,42 @@
 public class Solution {
     public int FindTheLongestBalancedSubstring(string s) {
     
-            if (string.IsNullOrWhiteSpace(s)) return 0;
-            var max_l = 0;
-            var zeros = 0;
-            var ones = 0;
-            foreach (var i in s)
+    if (string.IsNullOrWhiteSpace(s)) return 0;
+    var max_l = 0;
+    var zeros = 0;
+    var ones = 0;
+    var length = 0;
+    foreach (var i in s)
+    {
+        if (i == '1')
+        {
+            ones++;
+            if (ones >= zeros)
             {
-                if (i == '1')
+                if (ones == zeros)
                 {
-                    ones++;
-                    if (ones >= zeros)
-                    {
-                        if (ones == zeros)
-                        {
-                            max_l = max_l > ones * 2 ? max_l : ones * 2;
-                        }
-                        ones = zeros = 0;
-                    }
+                    length = ones * 2;
+                    max_l = Math.Max(max_l, length);
                 }
-                else if (i == '0')
-                {
-                    if(ones > 0)
-                    {
-                        max_l = max_l > ones * 2 ? max_l : ones * 2;
-                        ones = zeros = 0;
-                    }
-                    zeros++;
-                }
+                ones = zeros = 0;
             }
-            if (ones > 0 && zeros > 0)
+        }
+        else if (i == '0')
+        {
+            if(ones > 0)
             {
-                max_l = max_l > ones*2 ? max_l : ones * 2;
+                length = ones * 2;
+                max_l = Math.Max(max_l, length);
+                ones = zeros = 0;
             }
-            return max_l;
+            zeros++;
+        }
     }
+    if (ones > 0 && zeros > 0)
+    {
+        length = ones * 2;
+        max_l = Math.Max(max_l, length);
+    }
+    return max_l;
+}
 }
