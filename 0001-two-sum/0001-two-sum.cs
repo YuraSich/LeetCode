a78ea163fe65666597b849916e663d3835dc2daf
YuraSich <39992462+YuraSich@users.noreply.github.com>
2023-04-11 14:39:26 +0700

    Time: 165 ms (57.77%), Space: 43.9 MB (72.58%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.Length; i++){
+            if(Math.Abs(i) > Math.Abs(target)){
+                continue;
+            }
+            for(int j = i+1; j < nums.Length; j++){
+                if(nums[j] + nums[i] == target){
+                    return new int[] {i,j};
+                }
+            }
+        }
+         return new int[] {-1,-1};
+    }
+}
