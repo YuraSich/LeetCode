678243852466e41ac7ba264ce1f0b2fe44373798
YuraSich <39992462+YuraSich@users.noreply.github.com>
2023-04-16 10:48:50 +0700

    Time: 170 ms (50.00%), Space: 61.6 MB (50.00%) - LeetHub

---
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public int NumWays(string[] words, string target) {
+        int alphabet = 26;
+            int mod = 1000000007;
+            int m = target.Length, k = words[0].Length;
+            int[,] cnt = new int[alphabet,k];
+            for (int j = 0; j < k; j++)
+            {
+                foreach (string word in words)
+                {
+                    cnt[word[j] - 'a',j]++;
+                }
+            }
+            long[,] dp = new long[m + 1,k + 1];
+            dp[0,0] = 1;
+            for (int i = 0; i <= m; i++)
+            {
+                for (int j = 0; j < k; j++)
+                {
+                    if (i < m)
+                    {
+                        dp[i + 1,j + 1] += cnt[target[i] - 'a',j] * dp[i,j];
+                        dp[i + 1,j + 1] %= mod;
+                    }
+                    dp[i,j + 1] += dp[i,j];
+                    dp[i,j + 1] %= mod;
+                }
+            }
+            return (int)dp[m,k];
+    }
+}
