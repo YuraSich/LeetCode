eca2b5b668eb46bf360767b864ed912dfc29c35c
YuraSich <39992462+YuraSich@users.noreply.github.com>
2023-04-26 15:35:22 +0700

    Time: 33 ms (10.51%), Space: 26.3 MB (94.90%) - LeetHub

--- 0258-add-digits/0258-add-digits.cs
+++ 0258-add-digits/0258-add-digits.cs
@@ -1,9 +1,5 @@
 public class Solution {
     public int AddDigits(int num) {
-        var s = num;
-        while (s >= 10) {
-            s = s.ToString().Sum(c => c - '0');
-        }
-        return s;
+        return num == 0 ? 0 : 1 + (num - 1) % 9;
     }
 }
