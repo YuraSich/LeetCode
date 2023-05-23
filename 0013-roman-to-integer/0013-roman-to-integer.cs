43a58fc09344cd9b99c5c49e1b3cff6ca2cd82fd
YuraSich <39992462+YuraSich@users.noreply.github.com>
2023-05-23 19:02:20 +0700

    Time: 75 ms (70.96%), Space: 46.6 MB (77.66%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cs
+++ 0013-roman-to-integer/0013-roman-to-integer.cs
@@ -1,38 +1,20 @@
 public class Solution {
     public int RomanToInt(string s) {
-        int result = 0;
-        for (int i = 0; i < s.Length; i++)
-        {
-            char cur = s[i];
-            switch (cur)
+       int result = 0;
+            s = s.Replace("IV", "IIII").Replace("IX", "VIIII").Replace("XL", "XXXX").Replace("XC", "LXXXX").Replace("CD", "CCCC").Replace("CM", "DCCCC");
+            for (int i = 0; i < s.Length; i++)
             {
-                case 'I':
-                    result += 1;
-                    if (i < s.Length - 1)
-                    {
-                        if (s[i + 1] == 'V' || s[i + 1] == 'X') result -= 2;
-                    }
-                    break;
-                case 'V': result += 5; break;
-                case 'X':
-                    result += 10;
-                    if (i < s.Length - 1)
-                    {
-                        if (s[i + 1] == 'L' || s[i + 1] == 'C') result -= 20;
-                    }
-                    break;
-                case 'L': result += 50; break;
-                case 'C':
-                    result += 100;
-                    if (i < s.Length - 1)
-                    {
-                        if (s[i + 1] == 'D' || s[i + 1] == 'M') result -= 200;
-                    }
-                    break;
-                case 'D': result += 500; break;
-                case 'M': result += 1000; break;
+                switch (s[i])
+                {
+                    case 'I':result += 1;break;
+                    case 'V': result += 5; break;
+                    case 'X':result += 10;break;
+                    case 'L': result += 50; break;
+                    case 'C':result += 100;break;
+                    case 'D': result += 500; break;
+                    case 'M': result += 1000; break;
+                }
             }
-        }
-        return result;
+            return result;
     }
 }
