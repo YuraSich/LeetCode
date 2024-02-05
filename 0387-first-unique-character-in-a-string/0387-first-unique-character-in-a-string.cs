e542b5ad07cf56cb7f13dda24f3783270dc20daf
YuraSich <39992462+YuraSich@users.noreply.github.com>
2024-02-05 22:49:19 +0300

    Time: 56 ms (80.39%), Space: 46 MB (30.88%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cs
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cs
@@ -2,14 +2,19 @@ public class Solution {
     public int FirstUniqChar(string s) {
         var good = new HashSet<char>();
         var bad = new HashSet<char>();
-        foreach (var x in s.Where(x => !bad.Contains(x)))
-        {
-            if (!good.Add(x))
-            {
-                good.Remove(x);
-                bad.Add(x);
-            }
-        }
+         foreach (var x in s)
+         {
+             if (bad.Contains(x))
+             {
+                 continue;
+             }
+             if (!good.Add(x))
+             {
+                 good.Remove(x);
+                 bad.Add(x);
+             }
+
+         }
 
         return good.Count != 0 ? good.Min(s.IndexOf) : -1;
     }
