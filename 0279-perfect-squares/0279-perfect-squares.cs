488f74f4522f9c8a127968034f4fccf871bd8be2
YuraSich <39992462+YuraSich@users.noreply.github.com>
2024-02-08 20:59:05 +0300

    Time: 34 ms (90.97%), Space: 27.4 MB (88.89%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cs
+++ 0279-perfect-squares/0279-perfect-squares.cs
@@ -1,39 +1,38 @@
 public class Solution {
     public int NumSquares(int x) {
-        
-            // x = a^2
-            if (IsSquare(x))
-            {
-                return 1;
-            }
+        // x = a^2
+        if (IsSquare(x))
+        {
+            return 1;
+        }
 
-            // x = 4^k*(8*m + 7) => 4
-            for (int k = 0; k <= Math.Pow(x, 1 / 4f); k++)
+        // x = 4^k*(8*m + 7) => 4
+        for (int k = 0; k <= Math.Pow(x, 1 / 4f); k++)
+        {
+            for (int m = 0; m < x; m++)
             {
-                for (int m = 0; m < x; m++)
+                double r = Math.Pow(4, k) * (8 * m + 7);
+                if (r > x) continue;
+                if (r == x)
                 {
-                    double r = Math.Pow(4, k) * (8 * m + 7);
-                    if (r > x) continue;
-                    if (r == x)
-                    {
-                        return 4;
-                    }
+                    return 4;
                 }
             }
-
-            // x - i^2 = a^2 => 2
-            for (int i = 0; i < Math.Sqrt(x); i++)
-            {
-                if(IsSquare(x - (i * i))) return 2;
-            }
-
-
-            return 3;
         }
 
-        private static bool IsSquare(int x)
+        // x - i^2 = a^2 => 2
+        for (int i = 0; i < Math.Sqrt(x); i++)
         {
-            int y = (int)Math.Sqrt(x);
-            return y * y == x;
+            if(IsSquare(x - (i * i))) return 2;
         }
+
+
+        return 3;
     }
+
+    private static bool IsSquare(int x)
+    {
+        int y = (int)Math.Sqrt(x);
+        return y * y == x;
+    }
+}
