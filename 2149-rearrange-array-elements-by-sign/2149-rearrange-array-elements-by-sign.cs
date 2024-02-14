0a9cc945eac80a9477cfa9536211c8dda09912ae
YuraSich <39992462+YuraSich@users.noreply.github.com>
2024-02-14 22:19:46 +0300

    Time: 534 ms (16.33%), Space: 132.7 MB (62.25%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cs
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cs
@@ -1,15 +1,16 @@
 public class Solution {
     public int[] RearrangeArray(int[] nums)
     {
-        var rez = new List<int>();
+        var rez = new int[nums.Length];
         using var p = nums.Where(x => x > 0).GetEnumerator();
         using var n = nums.Where(x => x < 0).GetEnumerator();
+        var i = 0;
         while (p.MoveNext() & n.MoveNext())
         {
-            rez.Add(p.Current);
-            rez.Add(n.Current);
+            rez[i++] = p.Current;
+            rez[i++] = n.Current;
         }
 
-        return rez.ToArray();
+        return rez;
     }
 }
