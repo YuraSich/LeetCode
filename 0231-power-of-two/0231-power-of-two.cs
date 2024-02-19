2932e19540d3e6c27a85b4f82a9fbec3059b2607
YuraSich <39992462+YuraSich@users.noreply.github.com>
2024-02-19 21:17:53 +0300

    Time: 29 ms (68.05%), Space: 28.9 MB (15.40%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cs
+++ 0231-power-of-two/0231-power-of-two.cs
@@ -1,18 +1,5 @@
 public class Solution {
     public bool IsPowerOfTwo(int n) {
-        if(n <= 0)
-        {
-            return false;    
-        }
-        
-        for (var i = n; i > 1; i /= 2)
-        {
-            if (i % 2 != 0)
-            {
-                return false;
-            }
-        }
-
-        return true;
+        return n > 0 && (n & (n - 1)) == 0;
     }
 }
