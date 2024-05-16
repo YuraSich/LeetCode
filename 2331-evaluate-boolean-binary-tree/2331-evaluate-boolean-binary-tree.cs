aa546cb2ec0f0c54f74d6379e9350b2832c1410d
YuraSich <39992462+YuraSich@users.noreply.github.com>
2024-05-16 21:56:36 +0300

    Time: 100 ms (9.09%), Space: 47.4 MB (69.70%) - LeetHub

---
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.cs
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool EvaluateTree(TreeNode root) {
+         if (root.right == null && root.left == null)
+         {
+             return root.val == 1;
+         }
+         return root.val == 3 ?
+            EvaluateTree(root.left) & EvaluateTree(root.right) :
+            EvaluateTree(root.left) | EvaluateTree(root.right);
+    }
+}
